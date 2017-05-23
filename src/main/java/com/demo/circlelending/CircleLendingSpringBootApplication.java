f895d8b744a8ed2a2aa3779f01b2c99f7b50d0c1
aadari <adari.rajesh@yahoo.com>
2017-05-22 23:40:02 -0400

    pushing project created spring boot app fo circlelending

---
+++ src/main/java/com/demo/circlelending/CircleLendingSpringBootApplication.java
@@ -0,0 +1,14 @@
+package com.demo.circlelending;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CircleLendingSpringBootApplication
+{
+
+	public static void main(String[] args) 
+	{
+		SpringApplication.run(CircleLendingSpringBootApplication.class, args);
+	}
+}
---
+++ src/test/java/com/demo/circlelending/CircleLendingSpringBootApplicationTests.java
@@ -0,0 +1,16 @@
+package com.demo.circlelending;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CircleLendingSpringBootApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
